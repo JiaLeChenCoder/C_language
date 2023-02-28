c964adca2862aef1644962dd264dd041104a4a4d
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-02-28 21:18:48 +0800

    Add files via upload
    if_else语句及for循环的使用。

---
+++ 2_28_FindYValue/text.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+//int main()
+//{
+//int x;
+//scanf("%d", &x);
+//x > 0 ? printf("-1") : (x == 0 ? printf("0") : printf("1"));
+//return 0;
+//}
+
+//int main() {
+//    int value=0;
+//    scanf("%d",&value);
+//    if(value>=140)
+//    {
+//      printf("Genius");  
+//    }
+//    return 0;
+//}
+
+//int main() {
+//   int a=40,c=212,value=0;
+//   value=(-8+22)*a-10+c/2;
+//   printf("%d\n",value);
+//    return 0;
+//}
+
+int main() {
+    int i=0;
+   for(i=0;i<2;i++)
+   {
+    printf("     **     \n");
+   }
+   for(i=0;i<=2;i++)
+   {
+    printf("************\n");
+   }
+   for(i=0;i<2;i++)
+   {
+    printf("    *  *    \n");
+   }
+    return 0;
+}
