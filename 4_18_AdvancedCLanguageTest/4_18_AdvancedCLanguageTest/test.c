2c642dcc2083492f45303f5aa0b9f8d5815b7a18
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-04-18 23:26:55 +0800

    Add files via upload
    C语言测试题

---
+++ 4_18_AdvancedCLanguageTest/4_18_AdvancedCLanguageTest/test.c
@@ -0,0 +1,43 @@
+#define  _CRT_SECURE_NO_WARNINGS  1
+#include<stdio.h>
+//int main()
+//{
+//	char arr1[1000] = { 0 };
+//	char arr2[1000] = { 0 };
+//	gets(arr1);
+//	gets(arr2);
+//	int i = 0;
+//	while (arr1[i])
+//	{
+//		if (strchr(arr2,arr1[i]) == NULL)
+//		{
+//			printf("%c",arr1[i]);
+//		}
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	char a[15] = { 0 };
+//	scanf("%d", &n);
+//	int i = 0;
+//	int k = 0;
+//	while (n)
+//	{
+//		if (k != 0 && k % 3 == 0)
+//		{
+//			a[i++]=',';
+//		}
+//		a[i++] = n % 10+'0';
+//		n = n / 10;
+//		k++;
+//	}
+//	for (i = 14; i >=0; i--)
+//	{
+//		printf("%c", a[i]);
+//	}
+//}
+
