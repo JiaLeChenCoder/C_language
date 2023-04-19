3a95589247a817232d005eea2a557c63402bd93d
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-04-19 23:40:21 +0800

    Update test.c

--- 4_19_C/4_19_C/test.c
+++ 4_19_C/4_19_C/test.c
@@ -1,22 +1,4 @@
 #define _CRT_SECURE_NO_WARNINGS  1
-//#include"SList.h"
-//void Test1()
-//{
-//	SLTNode* plist = NULL;
-//	SLTPushFront(&plist, 1);
-//	SLTPushFront(&plist, 2);
-//	SLTPushFront(&plist, 3);
-//	SLTPushFront(&plist, 4);
-//	SLTPushFront(&plist, 5);
-//
-//	SLTPrint(plist);
-//}
-//int main()
-//{
-//	Test1();
-//	return 0;
-//}
-
 #include<stdio.h>
 #include<string.h>
 int main()
