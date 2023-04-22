d57a91cd068e4ab63ba3e1efdd5db891f25a6bf5
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-04-22 23:54:07 +0800

    Add files via upload
    单链表改进：
    头插法，尾插法，头删法，尾删法，打印，单链表查找，在pos之前插入，删除pos位置的值，删除pos位置后面的值。

--- 4_18_SingleLinkedList/4_18_SingleLinkedList/SList.h
+++ 4_18_SingleLinkedList/4_18_SingleLinkedList/SList.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<stdio.h>
 #include<stdlib.h>
+#include<assert.h>
 
 typedef int SLTDataType;
 typedef struct SListNode
@@ -11,3 +12,20 @@ typedef struct SListNode
 
 void SLTPrint(SLTNode* phead);
 void SLTPushFront(SLTDataType** Pphead, SLTDataType x);
+void SLPushBack(SLTNode** pphead, SLTDataType x);
+
+void SLPopFront(SLTNode** pphead);
+void SLPopBack(SLTNode** pphead);
+
+// 单链表查找
+SLTNode* STFind(SLTNode* phead, SLTDataType x);
+
+// 在pos之前插入
+void SLInsert(SLTNode** pphead, SLTNode* pos, SLTDataType x);
+void SLInsertAfter(SLTNode* pos, SLTDataType x);
+
+// 删除pos位置的值
+void SLErase(SLTNode** pphead, SLTNode* pos);
+
+// 删除pos位置后面的值
+void SLEraseAfter(SLTNode* pos);
--- 4_18_SingleLinkedList/4_18_SingleLinkedList/test.c
+++ 4_18_SingleLinkedList/4_18_SingleLinkedList/test.c
@@ -1,17 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS  1
 #include"SList.h"
-void Test1()
+//#include"SList.h"
+//void Test1()
+//{
+//	SLTNode* plist = NULL;
+//	SLTPushFront(&plist, 1);
+//	SLTPushFront(&plist, 2);
+//	SLTPushFront(&plist, 3);
+//	SLTPushFront(&plist, 4);
+//	SLTPushFront(&plist, 5);
+
+//	SLTPrint(plist);
+//}
+//int main()
+//{
+//	Test1();
+//	return 0;
+//}
+
+
+void TestSList4()
 {
 	SLTNode* plist = NULL;
-	SLTPushFront(&plist, 1);
-	SLTPushFront(&plist, 2);
-	SLTPushFront(&plist, 3);
-	SLTPushFront(&plist, 4);
-	SLTPushFront(&plist, 5);
+	SLPushBack(&plist, 1);
+	SLPushBack(&plist, 2);
+	SLPushBack(&plist, 3);
+	SLPushBack(&plist, 4);
+	SLTPrint(plist);
 
+	SLTNode* pos = STFind(plist, 3);
+	if (pos)
+	{
+		SLInsert(&plist, pos, 30);
+	}
+	SLTPrint(plist);
+
+	pos = STFind(plist, 2);
+	if (pos)
+	{
+		SLInsertAfter(pos, 20);
+	}
+	SLTPrint(plist);
+
+	pos = STFind(plist, 2);
+	if (pos)
+	{
+		SLErase(&plist, pos);
+	}
 	SLTPrint(plist);
 }
+
 int main()
 {
-	Test1();
+	TestSList4();
+
 	return 0;
 }
