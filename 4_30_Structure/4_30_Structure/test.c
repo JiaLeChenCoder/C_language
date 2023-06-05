b2e61cc7127a389cc06e7b24d76df8cc5f8b51b9
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-06-05 23:50:11 +0800

    Add files via upload
    结构体改进

--- 4_30_Structure/4_30_Structure/test.c
+++ 4_30_Structure/4_30_Structure/test.c
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS  1
 //实验10 结构体
+//修正版
 struct student
 {
 	long num;//学号
 	char name[30];//姓名
-	char gender;//性别
+	char gender[10];//性别
 	float score[3];//分数
 }s1;
-
 #include<stdio.h>
 int main()
 {
@@ -15,11 +15,11 @@ int main()
 	printf("请输入姓名\n");
 	gets(s1.name);
 	printf("请输入学号,性别\n");
-	scanf("%ld %c", &s1.num, &s1.gender);
+	scanf("%ld %s",&s1.num,&s1.gender);
 	printf("请输入3科分数\n");
-	for(i = 1;i<=3; i++)
+	for(i = 0;i<3; i++)
 	{
-		scanf("%f",s1.score[i]);
+		scanf("%f",&s1.score[i]);
 	}
 	printf("姓名:%s\n平均成绩:%lf\n",s1.name,(s1.score[0] + s1.score[1] + s1.score[2])/3);
 	return 0;
