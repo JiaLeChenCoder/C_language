d67eecb60199a96fa2c60638c140fda76a5bbc61
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-06-07 23:47:56 +0800

    Add files via upload

---
+++ 6_7/6_7/tesc.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//int main()
+//{
+//	/*int b = 1;
+//	int x = 0;
+//	if (b != 5) 
+//		x = 2;
+//	printf("%d", x);*/
+//	int x = 8, y = 10, z;
+//	z = ++x * y++;
+//	printf("%d",z);
+//
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+ z'' {
+	/*int n=1;
+	double s=0;
+	do{
+		s += 1.0 (2 * n - 1);
+		n++;
+	} while (s <= 4);
+	printf("n=%d\n", n-1);
+	return 0;*/
+	int a, b,c,d;
+  	printf("请输入两个整数\n");
+	scanf("%d,%d", &a, &b);
+	c = (a * a + b * b) / 100;
+	d = (a + b);
+	if (a * a + b * b > 100)
+		printf("%d", c);
+	else
+		printf("%d", d);
+	return 0;
+}
+
+
+
