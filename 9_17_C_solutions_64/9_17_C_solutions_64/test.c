0f4b5a7a7c90c0a08b1a322c28f4fe2d24f1df1c
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-09-17 23:09:24 +0800

    Add files via upload

---
+++ 9_17_C_solutions_64/9_17_C_solutions_64/test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+void NONO();
+
+int fun(int a[], int n)
+{
+	int i = 0, j = 0, k = 0, m, flag = 0, l = 0, sum = 0;
+	int b[10];
+	for (i = 0; i < n; i++)
+	{
+		j = 0;
+		while (a[i])
+		{
+			b[j++] = a[i] % 10;
+			a[i] = a[i] / 10;
+		}
+
+		for (k = 0; k < j; k++)
+		{
+			m = b[k];
+			for (l = k + 1; l < j; l++)
+			{
+				if (m == b[l])
+				{
+					flag = 1;
+				}
+			}
+		}
+		if (flag == 1)
+		{
+			sum++;
+			flag = 0;
+		}
+	}
+	return sum;
+
+}
+
+void main()
+{
+	int a[10] = { 2430,3001,1798,199,498,277,229,851,369,1146 };
+	int m;
+
+	m = fun(a, 10);
+	printf("符合条件的数共有：%d\n", m);
+	NONO();
+	getch();
+}
+
+void NONO()
+{
+	FILE* fp = fopen("in.dat", "r");
+	FILE* fw = fopen("out.dat", "w");
+	int a[5000];
+	int cnt = 0, m;
+
+	while (!feof(fp)) fscanf(fp, "%d", &a[cnt++]);
+	m = fun(a, cnt);
+	fprintf(fw, "%d\n", m);
+	fclose(fp); fclose(fw);
+}
