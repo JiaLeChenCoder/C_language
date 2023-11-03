6ed643d8d9c516fa91f8b35f35b7caf4df592be3
JiaLeChenCoder <121877226+JiaLeChenCoder@users.noreply.github.com>
2023-11-03 23:20:49 +0800

    Add files via upload

--- 11_2_linklist/11_2_linklist/test.cpp
+++ 11_2_linklist/11_2_linklist/test.cpp
@@ -24,19 +24,39 @@ void insret_head(Linklist &L)
 	}
 }
 
+void insret_tail(Linklist& L)
+{
+	int i = 0;
+	L = (Linklist)malloc(sizeof(Lnode));
+	L->next = NULL;
+	scanf("%d", &i);
+	Linklist m;
+	m = L;
+	while (i != 9999)
+	{
+		Linklist s = (Linklist)malloc(sizeof(Lnode));
+         		
+		s->data = i;
+		s->next = NULL;
+		m->next = s;
+		m = s;
+		scanf("%d", &i);
+	}
+}
 void print(Linklist L)
 {
 	L = L->next;
 	while (L)
 	{
-		printf("%d", L->data);
+		printf("%d ", L->data);
 		L = L->next;
 	}
 }
 int main()
 {
 	Linklist l;
-	insret_head(l);
+	//insret_head(l);
+	insret_tail(l);
 	print(l);
 	return 0;
 }
